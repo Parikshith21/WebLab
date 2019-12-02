40d30f781835c8cbe9cfba8e0a2f21fcab690d72
Parikshith Honnegowda <parikshithh@gmail.com>
2019-12-02 13:12:30 +0530

    Update time.php

--- time.php
+++ time.php
@@ -1,7 +1,7 @@
 <html>
 <head>
-<meta http-equiv="refresh" content="1"/>
-
-<p> <?php echo date("d/m/y::h:i:s A");?> </p>
+<p> <?php 
+date_default_timezone_set("Asia/Kolkata");
+echo date("d/m/y::h:i:s A");?> </p>
 </head>
 </html>
